11c941ebc5015370ab0d8096684c8b83e749c69c
ilhmsmith <90134448+ilhmsmith@users.noreply.github.com>
2025-02-21 07:46:57 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "24883626"  # Replace this API ID with your actual API ID
+API_HASH = "851cbf4ad3b9ebffdd244360b8c4511f"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "BQF7saoAm78lc4dVa7OOHDxWB_OPjn4slV2fbVgaq6kcc_mB6bo4yrg_Qp5tQy_hm3GbEUbrraeRCUgJ8864IAV7n2StQFirZnR1bBCxjQiyrANZ9--5fgd6tK2W2TBJit0yHrKjJoT74LsSDJ1-pI_wGW2Bsps-GK9gVmW6Tvc57a2niCMrx5AFq9FRaHcQynC4hvxKZurQKBev1IHTeknpYBSq-MTo8LzAMe2asU39aO0Hgx5Mmj2F2focpfg4zzaG6xDQqXH2LvoZtVF4QBFCe_X1C_tUxQr28LEwt_YpQIyskTuEfp-cOBpk1MS8nmYSTrd_3SKGGMdLSu9ndtYxSTtFAwAAAAFCkSCYAA"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "7237013474:AAGtCQzeoVTPYcf0YYkW8aSwIIjDfBpvNXE"  # Replace this BOT_TOKEN
+
+# Admin IDs
+ADMIN_IDS = [12345678, 12345678]
+
+# Limits
+DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
+ADMIN_LIMIT = 50000 # Card Scrapping Limit For Admin
